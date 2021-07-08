66cdfe64a4267da1b1cc32faaf0379a52b2be810
rainbowcow9 <23nidhyas@gmail.com>
2021-07-08 14:28:22 -0700

    added storage and containers

--- airplane_assembly_demo.py
+++ airplane_assembly_demo.py
@@ -5,8 +5,8 @@ import rospy
 import sys
 import time
 import numpy as np
-from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
-from adarrt import AdaRRT
+#from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
+#from adarrt import AdaRRT
 import pickle
 
 
@@ -44,7 +44,7 @@ def createTSR(partPose, hand):
     partTSR.set_T0_w(partPose)
 
     rot_trans = np.eye(4)
-    rot_trans[0:3, 0:3] = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    rot_trans[0:3, 0:3] = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
 
 
     partTSR_Tw_e = np.matmul(rot_trans, hand.get_endeffector_transform("cylinder"))
@@ -60,7 +60,7 @@ def closeHand(hand, displacement):
     hand.execute_preshape(displacement)
 
 
-roscpp_init('airplane_assembly', [])
+#roscpp_init('airplane_assembly', [])
 rospy.init_node("adapy_assembly")
 rate = rospy.Rate(10)
 
@@ -74,30 +74,70 @@ if not rospy.is_shutdown():
 
     viewer = ada.start_viewer("dart_markers/simple_trajectories", "map")
 
-    obj1URDFUri = "package://pr_assets/data/objects/containerBig.urdf"
+    obj1URDFUri = "package://pr_assets/data/objects/container_3.urdf"
     object1Pose = [0.21, 0.44, 0.038, 0.707, 0., 0., 0.707]
 
-    obj2URDFUri = "package://pr_assets/data/objects/containerBig.urdf"
+    obj2URDFUri = "package://pr_assets/data/objects/container_3.urdf"
     object2Pose = [0.00, 0.44, 0.038, 0.707, 0., 0., 0.707]  # [0.00, -0.5, 0.012, 0.707, 0., 0., 0.707]
 
-    obj3URDFUri = "package://pr_assets/data/objects/containerMid.urdf"
+    obj3URDFUri = "package://pr_assets/data/objects/container_2.urdf"
     object3Pose = [-0.14, 0.42, 0.032, 0.707, 0., 0., 0.707]
 
-    obj4URDFUri = "package://pr_assets/data/objects/containerMid.urdf"
+    obj4URDFUri = "package://pr_assets/data/objects/container_2.urdf"
     object4Pose = [-0.24, 0.44, 0.032, 0.707, 0., 0., 0.707]
 
-    obj5URDFUri = "package://pr_assets/data/objects/containerMid.urdf"
+    obj5URDFUri = "package://pr_assets/data/objects/container_2.urdf"
     object5Pose = [0.00, 0.62, 0.095, 0.707, 0., 0., 0.707]
 
-    obj6URDFUri = "package://pr_assets/data/objects/containerMid.urdf"
+    obj6URDFUri = "package://pr_assets/data/objects/container_2.urdf"
     object6Pose = [-0.10, 0.58, 0.095, 0.707, 0., 0., 0.707]
+
+    wingURDFUri = "package://pr_assets/data/objects/abstract_main_wing.urdf"
+    wingPose = [0, 0.5, 0.1, 0.7071068, 0, 0, -0.7071068]
+
+    storageURDFUri = "package://pr_assets/data/objects/storage.urdf"
+    storagePose = [3, 0, 0, 0, 0, 0, 0]
+    
+
+    container2_1URDFUri = "package://pr_assets/data/objects/container_2.urdf"
+    container2_1Pose = [0.2275, -0.1, 0.4, 0.7071068, 0, 0, 0.7071068]
+
+    container1_1URDFUri = "package://pr_assets/data/objects/container_1.urdf"
+    container1_1Pose = [3.2975, 0.5, 0.7825, 0, 0, 0, 0]
+    
+    container3_1URDFUri = "package://pr_assets/data/objects/container_3.urdf"
+    container3_1Pose = [3.1275, 0.5, 0.7825, 0, 0, 0, 0]
+    
+    #container1_2URDFUri = "package://pr_assets/data/objects/container_1.urdf"
+    container1_2Pose = [2.9575, 0.5, 0.7825, 0, 0, 0, 0]
+
+    container3_2Pose = [2.7875, 0.5, 0.7825, 0, 0, 0, 0]
+
+    container1_3Pose = [2.6175, 0.5, 0.7825, 0, 0, 0, 0]
+
+    container2_2Pose = [2.4875, 0.5, 0.7825, 0, 0, 0, 0]
+
+    container1_3Pose = [2.3575, 0.5, 0.7825, 0, 0, 0, 0]
+    
 
     world = ada.get_world()
 
-    obj1 = world.add_body_from_urdf(obj1URDFUri, object1Pose)
-    obj2 = world.add_body_from_urdf(obj2URDFUri, object2Pose)
-    obj3 = world.add_body_from_urdf(obj3URDFUri, object3Pose)
-    obj4 = world.add_body_from_urdf(obj4URDFUri, object4Pose)
+    #obj1 = world.add_body_from_urdf(obj1URDFUri, object1Pose)
+    #obj2 = world.add_body_from_urdf(obj2URDFUri, object2Pose)
+    #obj3 = world.add_body_from_urdf(obj3URDFUri, object3Pose)
+    #obj4 = world.add_body_from_urdf(obj4URDFUri, object4Pose)
+    wing = world.add_body_from_urdf(wingURDFUri, wingPose)
+    storageInWorld = world.add_body_from_urdf(storageURDFUri, storagePose)
+    container2_1 = world.add_body_from_urdf(container2_1URDFUri, container2_1Pose)
+    container1_1 = world.add_body_from_urdf(container1_1URDFUri, container1_1Pose)
+    container3_1 = world.add_body_from_urdf(container3_1URDFUri, container3_1Pose)
+    container1_2 = world.add_body_from_urdf(container1_1URDFUri, container1_2Pose)
+    container3_2 = world.add_body_from_urdf(container3_1URDFUri, container3_2Pose)
+    container1_3 = world.add_body_from_urdf(container1_1URDFUri, container1_3Pose)
+    container2_2 = world.add_body_from_urdf(container2_1URDFUri, container2_2Pose)
+    container1_3 = world.add_body_from_urdf(container1_1URDFUri, container1_3Pose)
+
+
 
     rospy.sleep(5.0)
 
@@ -121,7 +161,7 @@ if not rospy.is_shutdown():
     # ----------------------- Create TSR for grasping ----------------------- #
 
 
-    choosed_objectPose = object6Pose
+    choosed_objectPose = wingPose
 
     objectPoseMat = [[1.0, 0.0, 0.0, choosed_objectPose[0]],
                       [0.0, 1.0, 0.0, choosed_objectPose[1]],
@@ -131,6 +171,96 @@ if not rospy.is_shutdown():
 
 
     marker = viewer.add_tsr_marker(objectTSR)
+
+    choosed_object2Pose = container2_1Pose
+
+    object2PoseMat = [[0.0, 0.0, 1.0, choosed_object2Pose[0]],
+                      [0.0, -1.0, 0.0, choosed_object2Pose[1]],
+                      [1.0, 0.0, 0.0, choosed_object2Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object2TSR = createTSR(object2PoseMat,hand)
+
+
+    marker2 = viewer.add_tsr_marker(object2TSR)
+
+    choosed_object3Pose = container1_1Pose
+
+    object3PoseMat = [[0.0, 0.0, 1.0, choosed_object3Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object3Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object3Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object3TSR = createTSR(object3PoseMat,hand)
+
+
+    marker3 = viewer.add_tsr_marker(object3TSR)
+
+    choosed_object4Pose = container3_1Pose
+
+    object4PoseMat = [[0.0, 0.0, 1.0, choosed_object4Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object4Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object4Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object4TSR = createTSR(object4PoseMat,hand)
+
+
+    marker4 = viewer.add_tsr_marker(object4TSR)
+
+    choosed_object5Pose = container1_2Pose
+
+    object5PoseMat = [[0.0, 0.0, 1.0, choosed_object5Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object5Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object5Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object5TSR = createTSR(object5PoseMat,hand)
+
+
+    marker5 = viewer.add_tsr_marker(object5TSR)
+
+    choosed_object6Pose = container3_2Pose
+
+    object6PoseMat = [[0.0, 0.0, 1.0, choosed_object6Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object6Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object6Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object6TSR = createTSR(object6PoseMat,hand)
+
+
+    marker6 = viewer.add_tsr_marker(object6TSR)
+
+    choosed_object7Pose = container1_3Pose
+
+    object7PoseMat = [[0.0, 0.0, 1.0, choosed_object7Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object7Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object7Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object7TSR = createTSR(object7PoseMat,hand)
+
+
+    marker7 = viewer.add_tsr_marker(object7TSR)
+
+    choosed_object8Pose = container2_2Pose
+
+    object8PoseMat = [[0.0, 0.0, 1.0, choosed_object8Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object8Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object8Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object8TSR = createTSR(object8PoseMat,hand)
+
+
+    marker8 = viewer.add_tsr_marker(object8TSR)
+
+    choosed_object9Pose = container1_3Pose
+
+    object9PoseMat = [[0.0, 0.0, 1.0, choosed_object9Pose[0]],
+                      [0.0, 1.0, 0.0, choosed_object9Pose[1]],
+                      [-1.0, 0.0, 0.0, choosed_object9Pose[2]],
+                      [0.0, 0.0, 0.0, 1.0]]
+    object9TSR = createTSR(object9PoseMat,hand)
+
+
+    marker9 = viewer.add_tsr_marker(object9TSR)
+
+
 
      # ------------------ Move robot to start configuration ----------------- #
 
@@ -148,7 +278,7 @@ if not rospy.is_shutdown():
 
     # ------------------------ Setup IK for grasping ------------------------ #
     
-    ik_sampleable = adapy.create_ik(arm_skeleton, arm_state_space, objectTSR, hand_node)
+    ik_sampleable = adapy.create_ik(arm_skeleton, arm_state_space, object2TSR, hand_node)
     ik_generator = ik_sampleable.create_sample_generator()
     configurations = []
     samples = 0
@@ -170,12 +300,12 @@ if not rospy.is_shutdown():
 
         #ada.set_arm_positions(configurations[0])
 
-        collision_free_constraint = ada.set_up_collision_detection(ada.get_arm_state_space(),
-                                                                            ada.get_arm_skeleton(),
-                                                                            [obj1,obj2,obj3,obj4])
-        full_collision_constraint = ada.get_full_collision_constraint(ada.get_arm_state_space(),
-                                                                              ada.get_arm_skeleton(),
-                                                                              collision_free_constraint)
+        #collision_free_constraint = ada.set_up_collision_detection(ada.get_arm_state_space(),
+         #                                                                   ada.get_arm_skeleton(),
+         #                                                                   [obj1,obj2,obj3,obj4])
+        #full_collision_constraint = ada.get_full_collision_constraint(ada.get_arm_state_space(),
+        #                                                                      ada.get_arm_skeleton(),
+        #                                                                      collision_free_constraint)
         collision = ada.get_self_collision_constraint()
 
         # trajectory = None
