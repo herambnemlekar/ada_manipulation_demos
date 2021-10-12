a3d00cf36dfb4049140715108e94a2ca9205a856
hsnemlekar <nemlekar@usc.edu>
2021-10-11 17:07:53 -0700

    anticipation integrated, action prediction todo

--- demos/airplane_assembly_demo.py
+++ demos/airplane_assembly_demo.py
@@ -8,6 +8,7 @@ import pickle
 import numpy as np
 from adarrt import AdaRRT
 from gui import *
+from assembly_tasks import *
 
 # set to False if operating real robot
 sim = True
@@ -56,7 +57,7 @@ def createTSR(partPose, adaHand):
     rot_trans = np.eye(4)
     rot_trans[0:3, 0:3] = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
     partTSR_Tw_e = np.matmul(rot_trans, adaHand.get_endeffector_transform("cylinder"))
-    partTSR_Tw_e[2] += 0.05
+    partTSR_Tw_e[2] += 0.0
 
     # set the transformed TSR
     partTSR.set_Tw_e(partTSR_Tw_e)
@@ -68,6 +69,36 @@ def createTSR(partPose, adaHand):
 
 def toggleHand(adaHand, displacement):
     adaHand.execute_preshape(displacement)
+
+
+def transition(s_from, a):
+    # preconditions
+    if a in [0, 1] and s_from[a] < 1:
+        p = 1.0
+    elif a == 2 and s_from[a] < 4 and s_from[0] == 1:
+        p = 1.0
+    elif a == 3 and s_from[a] < 1 and s_from[1] == 1:
+        p = 1.0
+    elif a == 4 and s_from[a] < 4 and s_from[a] + 1 <= s_from[a - 2]:
+        p = 1.0
+    elif a == 5 and s_from[a] < 1 and s_from[a] + 1 <= s_from[a - 2]:
+        p = 1.0
+    elif a == 6 and s_from[a] < 4:
+        p = 1.0
+    elif a == 7 and s_from[a] < 1 and s_from[a - 1] == 4:
+        p = 1.0
+    else:
+        p = 0.0
+
+    # transition to next state
+    if p == 1.0:
+        s_to = deepcopy(s_from)
+        s_to[a] += 1
+        s_to[-1] = s_from[-2]
+        s_to[-2] = a
+        return p, s_to
+    else:
+        return p, None
 
 
 # ------------------------------------------------------- MAIN ------------------------------------------------------- #
@@ -120,7 +151,16 @@ if not rospy.is_shutdown():
     container3_1 = world.add_body_from_urdf(container3URDFUri, container3_1Pose)
     container3_2 = world.add_body_from_urdf(container3URDFUri, container3_2Pose)
 
-    rospy.sleep(3.0)
+    # dict of all objects
+    objects = {"main wing": [wing, wingPose],
+               "long bolts": [container1_1, container1_1Pose],
+               "short bolts": [container1_2, container1_2Pose],
+               "propeller nut": [container1_3, container1_3Pose],
+               "tail bolt": [container1_4, container1_4Pose],
+               "propellers": [container2_1, container2_1Pose],
+               "tool": [container2_2, container2_2Pose],
+               "propeller hub": [container3_1, container3_1Pose],
+               "tail wing": [container3_2, container3_2Pose]}
 
     # ------------------------------------------------ Get robot config ---------------------------------------------- #
 
@@ -144,157 +184,231 @@ if not rospy.is_shutdown():
     trajectory = ada.compute_joint_space_path(arm_state_space, waypoints)
     ada.execute_trajectory(trajectory)
     toggleHand(hand, [0.5, 0.5])
-    time.sleep(3)
+  
+    # ---------------------------------------------- Anticipation stuff ---------------------------------------------- #
+
+    qf = pickle.load(open("q_values.p", "rb"))
+    states = pickle.load(open("states.p", "rb"))
+
+    remaining_user_actions = [0, 1, 2, 3, 4, 5, 6, 7]
+    required_objects = [["main wing"],
+                        ["tail wing"],
+                        ["long bolts"],
+                        ["tail bolt"],
+                        ["tool"],
+                        ["tool"],
+                        ["propellers", "propeller hub", "short bolts", "tool"],
+                        ["propeller nut"]]
+    
+    current_state = states[0]
+    sensitivity = 0.0
 
     # --------------------------------------------------- MAIN Loop -------------------------------------------------- #
-
-    objects = {"main wing": [wing, wingPose],
-               "long bolts": [container1_1, container1_1Pose],
-               "short bolts": [container1_2, container1_2Pose],
-               "propeller nut": [container1_3, container1_3Pose],
-               "tail bolt": [container1_4, container1_4Pose],
-               "propellers": [container2_1, container2_1Pose],
-               "tool": [container2_2, container2_2Pose],
-               "propeller hub": [container3_1, container3_1Pose]}
 
     # initialize gui
     app = QApplication(sys.argv)
 
     # loop over all objects
     remaining_objects = objects.keys()
-    while remaining_objects:
+
+    for step in range(17):
+        
+        # anticipate user action
+        s = states.index(current_state)
+        max_action_val = -np.inf
+        candidates = []
+        applicants = []
+        for a in remaining_user_actions:
+            p, sp = transition(states[s], a)
+            if sp:
+                applicants.append(a)
+                if qf[s][a] > (1 + sensitivity) * max_action_val:
+                    candidates = [a]
+                    max_action_val = qf[s][a]
+                elif (1 - sensitivity) * max_action_val <= qf[s][a] <= (1 + sensitivity) * max_action_val:
+                    candidates.append(a)
+                    max_action_val = qf[s][a]
+
+        anticipated_actions = list(set(candidates))
+        available_actions = list(set(applicants))
+
+
+        # objects required for anticipated actions
+        available_objects = []
+        suggested_objects = []
+        for a in available_actions:
+            available_objects += required_objects[a]
+            if a in anticipated_actions:
+                suggested_objects += required_objects[a]
+        available_objects = list(set(available_objects))
+        suggested_objects = list(set(suggested_objects))
+
         win = UserInterface()
-        win.set_options(remaining_objects)
+        win.set_options(available_objects, suggested_objects)
     
-        while not win.user_choice:
+        while not win.act:
             win.show()
             app.exec_()
 
-        print("Robot will fetch:", win.user_choice)
-
-        obj = objects[win.user_choice][0]
-        objPose = objects[win.user_choice][1]
-        objPoseMat = [[1.0, 0.0, 0.0, objPose[0]],
-                      [0.0, 1.0, 0.0, objPose[1]],
-                      [0.0, 0.0, 1.0, objPose[2]],
-                      [0.0, 0.0, 0.0, 1.0]]
-        objTSR = createTSR(objPoseMat, hand)
-        # marker = viewer.add_tsr_marker(objTSR)
-
-        # -------------------------------------------- Collision detection ----------------------------------------------- #
-
-        # collision_free_constraint = ada.set_up_collision_detection(ada.get_arm_state_space(), ada.get_arm_skeleton(),
-        #                                                                        [obj])
-        # full_collision_constraint = ada.get_full_collision_constraint(ada.get_arm_state_space(),
-        #                                                                      ada.get_arm_skeleton(),
-        #                                                                      collision_free_constraint)
-        # collision = ada.get_self_collision_constraint()
-
-
-        # ------------------------------------------- Setup IK for grasping ---------------------------------------------- #
-        
-        ik_sampleable = adapy.create_ik(arm_skeleton, arm_state_space, objTSR, hand_node)
-        ik_generator = ik_sampleable.create_sample_generator()
-        configurations = []
-        samples = 0
-        maxSamples = 10
-        print("Finding IK configuration...")
-        while samples < maxSamples and ik_generator.can_sample():
-            goal_state = ik_generator.sample(arm_state_space)
-            samples += 1
-            if len(goal_state) == 0:
-                continue
-            configurations.append(goal_state)   
-
-        # ------------------------------------------- Plan path for grasping -------------------------------------------- #
-        
-        if len(configurations) == 0:
-            print("No valid configurations found!")
+        if win.user_choice:
+            objects_to_deliver = win.user_choice
         else:
-            print("IK Configuration found!")
+            objects_to_deliver = suggested_objects
 
-            collision = ada.get_self_collision_constraint()
+        for chosen_obj in objects_to_deliver:
 
-            # trajectory = None
-            # adaRRT = AdaRRT(start_state=np.array(armHome), goal_state=np.array(configuration[0]), step_size=0.05,
-            #                         goal_precision=0.1, ada=ada, objects=[storageInWorld])
-            # path = adaRRT.build()
-            # trajectory = None
-            # if path is not None:
-            #     waypoints = []
-            #     for i, waypoint in enumerate(path):
-            #         waypoints.append((0.0 + i, waypoint))
-            #     trajectory = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)  # 3
-           
-            waypoints = [(0.0,armHome),(1.0,configurations[0])]
-            trajectory = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
+            obj = objects[chosen_obj][0]
+            objPose = objects[chosen_obj][1]
+            objPoseMat = [[1.0, 0.0, 0.0, objPose[0]],
+                          [0.0, 1.0, 0.0, objPose[1]],
+                          [0.0, 0.0, 1.0, objPose[2]],
+                          [0.0, 0.0, 0.0, 1.0]]
+            objTSR = createTSR(objPoseMat, hand)
+            # marker = viewer.add_tsr_marker(objTSR)
 
-            # tFile = open("trojectCSV/1.txt","w")
-            # pickle.dump(trajectory,tFile)
-            # tFile.close()
+            # -------------------------------------------- Collision detection ----------------------------------------------- #
 
-            # ------------------------------------------ Execute path to grasp object --------------------------------- #
+            # collision_free_constraint = ada.set_up_collision_detection(ada.get_arm_state_space(), ada.get_arm_skeleton(),
+            #                                                                        [obj])
+            # full_collision_constraint = ada.get_full_collision_constraint(ada.get_arm_state_space(),
+            #                                                                      ada.get_arm_skeleton(),
+            #                                                                      collision_free_constraint)
+            # collision = ada.get_self_collision_constraint()
 
-            if not trajectory:
-                print("Failed to find a solution!")
+
+            # ------------------------------------------- Setup IK for grasping ---------------------------------------------- #
+            
+            ik_sampleable = adapy.create_ik(arm_skeleton, arm_state_space, objTSR, hand_node)
+            ik_generator = ik_sampleable.create_sample_generator()
+            configurations = []
+            samples = 0
+            maxSamples = 10
+            print("Finding IK configuration...")
+            while samples < maxSamples and ik_generator.can_sample():
+                goal_state = ik_generator.sample(arm_state_space)
+                samples += 1
+                if len(goal_state) == 0:
+                    continue
+                configurations.append(goal_state)   
+
+            # ------------------------------------------- Plan path for grasping -------------------------------------------- #
+            
+            if len(configurations) == 0:
+                print("No valid configurations found!")
             else:
-                ada.execute_trajectory(trajectory)
-                toggleHand(hand, [1.5, 1.5])
-                time.sleep(3)
-                hand.grab(obj)
+                print("IK Configuration found!")
 
-                # ----------------------- Lift up grasped object using Jacobian pseudo-inverse ------------------------ #
+                collision = ada.get_self_collision_constraint()
 
-                # lift up
-                waypoints = []
-                for i in range(5):
-                    jac = arm_skeleton.get_linear_jacobian(hand_node)
-                    full_jac = arm_skeleton.get_jacobian(hand.get_endeffector_body_node())
-                    delta_x = np.array([0, 0, 0, 0, 0, -0.05])
-                    delta_q = np.matmul(np.linalg.pinv(full_jac), delta_x)
-                    q = arm_skeleton.get_positions()
-                    upWaypt = q + delta_q
-                    waypoints.append((i, upWaypt))
-                    ada.set_positions(upWaypt)
+                # trajectory = None
+                # adaRRT = AdaRRT(start_state=np.array(armHome), goal_state=np.array(configuration[0]), step_size=0.05,
+                #                         goal_precision=0.1, ada=ada, objects=[storageInWorld])
+                # path = adaRRT.build()
+                # trajectory = None
+                # if path is not None:
+                #     waypoints = []
+                #     for i, waypoint in enumerate(path):
+                #         waypoints.append((0.0 + i, waypoint))
+                #     trajectory = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)  # 3
+               
+                waypoints = [(0.0,armHome),(1.0,configurations[0])]
+                trajectory = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
 
-                # print("Here.", waypoints)
-                traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
-                ada.execute_trajectory(traj)
-                time.sleep(3)
+                # tFile = open("trojectCSV/1.txt","w")
+                # pickle.dump(trajectory,tFile)
+                # tFile.close()
 
-                # --------------- Move grasped object to workbench -------------- #
+                # ------------------------------------------ Execute path to grasp object --------------------------------- #
 
-                # move forward
-                waypoints = []
-                for i in range(5):
-                    jac = arm_skeleton.get_linear_jacobian(hand_node)
-                    full_jac = arm_skeleton.get_jacobian(hand.get_endeffector_body_node())
-                    delta_x = np.array([0, 0, 0, 0, -0.05, 0])
-                    delta_q = np.matmul(np.linalg.pinv(full_jac), delta_x)
-                    q = arm_skeleton.get_positions()
-                    forWaypt = q + delta_q
-                    waypoints.append((i, forWaypt))
-                    ada.set_positions(forWaypt)
+                if not trajectory:
+                    print("Failed to find a solution!")
+                else:
+                    ada.execute_trajectory(trajectory)
+                    toggleHand(hand, [1.5, 1.5])
+                    time.sleep(2)
+                    hand.grab(obj)
 
-                traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
-                ada.execute_trajectory(traj)
-                time.sleep(3)
+                    # ----------------------- Lift up grasped object using Jacobian pseudo-inverse ------------------------ #
 
-                hand.ungrab(obj)
-                toggleHand(hand, [0.15, 0.15])
-                world.remove_skeleton(obj)
-                time.sleep(1)
+                    # lift up
+                    waypoints = []
+                    for i in range(5):
+                        jac = arm_skeleton.get_linear_jacobian(hand_node)
+                        full_jac = arm_skeleton.get_jacobian(hand.get_endeffector_body_node())
+                        delta_x = np.array([0, 0, 0, 0, 0, -0.05])
+                        delta_q = np.matmul(np.linalg.pinv(full_jac), delta_x)
+                        q = arm_skeleton.get_positions()
+                        upWaypt = q + delta_q
+                        waypoints.append((i, upWaypt))
+                        ada.set_positions(upWaypt)
 
-                # ------------------- Move robot back to home ------------------- #
+                    print("Picking up.")
+                    traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
+                    ada.execute_trajectory(traj)
 
-                waypoints = [(0.0, arm_skeleton.get_positions()), (1.0, armHome)]
-                traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
-                ada.execute_trajectory(traj)
-                time.sleep(2)
+                    # --------------- Move grasped object to workbench -------------- #
+
+                    # move forward
+                    # waypoints = []
+                    # for i in range(9):
+                    #     jac = arm_skeleton.get_linear_jacobian(hand_node)
+                    #     full_jac = arm_skeleton.get_jacobian(hand.get_endeffector_body_node())
+                    #     delta_x = np.array([0, 0, 0, 0, -0.05, 0])
+                    #     delta_q = np.matmul(np.linalg.pinv(full_jac), delta_x)
+                    #     q = arm_skeleton.get_positions()
+                    #     forWaypt = q + delta_q
+                    #     waypoints.append((i, forWaypt))
+                    #     ada.set_positions(forWaypt)
+
+                    current_position = arm_skeleton.get_positions()
+                    new_position = current_position.copy()
+                    new_position[0] -= 0.5
+                    waypoints = [(0.0, current_position), (1.0, new_position)]
+                    print("Moving forward.")
+                    traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
+                    ada.execute_trajectory(traj)
+
+                    # ----------------------- Lower grasped object using Jacobian pseudo-inverse ------------------------ #
+
+                    # lower
+                    waypoints = []
+                    for i in range(5):
+                        jac = arm_skeleton.get_linear_jacobian(hand_node)
+                        full_jac = arm_skeleton.get_jacobian(hand.get_endeffector_body_node())
+                        delta_x = np.array([0, 0, 0, 0, 0, 0.05])
+                        delta_q = np.matmul(np.linalg.pinv(full_jac), delta_x)
+                        q = arm_skeleton.get_positions()
+                        upWaypt = q + delta_q
+                        waypoints.append((i, upWaypt))
+                        ada.set_positions(upWaypt)
+
+                    print("Keeping down.")
+                    traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
+                    ada.execute_trajectory(traj)
+
+                    hand.ungrab(obj)
+                    toggleHand(hand, [0.15, 0.15])
+                    world.remove_skeleton(obj)
+                    time.sleep(1)
+
+                    # ------------------- Move robot back to home ------------------- #
+
+                    waypoints = [(0.0, arm_skeleton.get_positions()), (1.0, armHome)]
+                    traj = ada.compute_joint_space_path(ada.get_arm_state_space(), waypoints)
+                    ada.execute_trajectory(traj)
 
         print("Finished executing action.")
-        remaining_objects.remove(win.user_choice)
-        win.user_choice = None
+        remaining_objects = [rem_obj for rem_obj in remaining_objects if rem_obj not in objects_to_deliver]
+
+        match_score = 0
+        for a in remaining_user_actions:
+            a_score = len(set(objects_to_deliver).intersection(required_objects[a]))
+            if a_score >= match_score:
+                match_score = a_score
+                user_action = a
+
+        remaining_user_actions.remove(user_action)
+        _, current_state = transition(current_state, user_action) 
 
     # --------- Stop executor for real robot (not needed for sim) ----------- #
     if not sim:
--- src/gui.py
+++ src/gui.py
@@ -9,18 +9,19 @@ class UserInterface(QMainWindow):
 
         # window title and size
         self.setWindowTitle("Robot Commander")
-        self.setGeometry(250, 250, 500, 550)
+        self.setGeometry(250, 250, 500, 600)
 
         # prompt
         self.query = QLabel(self)
-        self.query.setText("Select the next robot action:")
+        self.query.setText("Select the next robot actions:")
         self.query.setFont(QFont('Arial', 14))
         self.query.adjustSize()
-        self.query.move(100, 50)
+        self.query.move(120, 50)
 
         # inputs
         self.options = []
-        self.user_choice = None
+        self.user_choice = []
+        self.act = False
 
     def set_options(self, options, suggestions=[]):
         option_x = 125
@@ -32,18 +33,39 @@ class UserInterface(QMainWindow):
             buttons[-1].setFont(QFont('Arial', 12))
             buttons[-1].setGeometry(option_x, option_y, 225, 50)
             buttons[-1].setCheckable(True)
-            buttons[-1].clicked.connect(self.set_choice)
+            # buttons[-1].clicked.connect(self.set_choice)
             if opt in suggestions:
-                button.setStyleSheet("background-color : yellow")
-            option_y += 50
-
+                buttons[-1].setStyleSheet("background-color : yellow")
+            option_y += 50    
         self.options = buttons
 
+        option_x = 100
+        option_y += 50
+        self.suggested_button = QPushButton(self)
+        self.suggested_button.setText("Perform the SUGGESTED actions")
+        self.suggested_button.setFont(QFont('Arial', 12))
+        self.suggested_button.setGeometry(option_x, option_y, 275, 50)
+        self.suggested_button.setStyleSheet("background-color : yellow")
+        self.suggested_button.setCheckable(True)
+        self.suggested_button.clicked.connect(self.set_choice)
+
+        option_x = 100
+        option_y += 50
+        self.selected_button = QPushButton(self)
+        self.selected_button.setText("Perform the SELECTED actions")
+        self.selected_button.setFont(QFont('Arial', 12))
+        self.selected_button.setGeometry(option_x, option_y, 275, 50)
+        self.selected_button.setCheckable(True)
+        self.selected_button.clicked.connect(self.set_choice)
+
     def set_choice(self):
-        for option in self.options:
-            if option.isChecked():
-                self.user_choice = option.text()
-                option.toggle()
+        self.act = True
+        
+        if self.selected_button.isChecked():
+            for option in self.options:
+                if option.isChecked():
+                    self.user_choice.append(option.text())
+                    # option.toggle()
 
         self.close()
 
